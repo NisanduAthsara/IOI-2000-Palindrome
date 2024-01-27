879f720ade1ff4578f0027c1460a327acf0f66ce
Nisandu Athsara <88542400+NisanduAthsara@users.noreply.github.com>
2024-01-27 11:19:24 +0530

    Change the code fully

--- v2.cpp
+++ v2.cpp
@@ -1,56 +1,24 @@
-#include <algorithm> 
-#include<iostream>
-#include<string>
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
+
 int main()
 {
-    int count = 0;
-    string word,updatedWord;
-    cin >> word;
-    updatedWord = word;
-    
-    for(int i=0;i<word.length();i++){
-        char letter = word[i];
-        int posInOtherSide = (word.length()-1)-i;
-        char letterInOtherSide = word[posInOtherSide];
-        if(letter != letterInOtherSide){
-            count += 1;
-            string backupWord;
-            for(int y=0;y<(updatedWord.length()+1);y++){
-                if(y == posInOtherSide){
-                    backupWord = backupWord + updatedWord[y];
-                    backupWord = backupWord + letter;
-                    
-                    string reverseStr = backupWord;
-                    reverse(reverseStr.begin(), reverseStr.end());
-                    if(reverseStr == backupWord){
-                        updatedWord = backupWord;
-                        cout << "Step " << count << " - " << backupWord << endl;
-                        cout << count << endl;
-                        cout << "Palindrome Word - " << updatedWord << endl;
-                        exit(0);
-                    }
-                }else{
-                    backupWord = backupWord + updatedWord[y];
-                    string reverseStr = backupWord;
-                    reverse(reverseStr.begin(), reverseStr.end());
-                    if(reverseStr == backupWord && y > 1){
-                        updatedWord = backupWord;
-                        cout << "Step " << count << " - " << backupWord << endl;
-                        cout << count << endl;
-                        cout << "Palindrome Word - " << updatedWord << endl;
-                        exit(0);
-                    }
-                }
-            }
-            updatedWord = backupWord;
-            cout << "Step " << count << " - " << backupWord << endl;
+    int n;
+    string s;
+    cin>>n>>s;
+    string s1=s,s2=s;
+    reverse(s2.begin(),s2.end());
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
     }
-    cout << count << endl;
-    cout << "Palindrome Word - " << updatedWord << endl;
+    cout << n - dp[n][n]<<endl;;
 
     return 0;
 }
